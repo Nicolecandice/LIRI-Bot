e4471c62ad6f0e5bcf0289ba2026ebdc96313fb5
Nicole Cooper <nicolecooper@Nicoles-MBP.home>
2018-05-03 05:09:43 -0400

    update

--- liri.js
+++ liri.js
@@ -1,75 +1,90 @@
-// import { twitter } from "./keys";
+ import { twitter } from "./keys";
 
-require("dotenv").config();
+ require("dotenv").config();
 
-// var spotify = new spotify(keys.spotify);
-// var client = new twitter(keys.twitter);
+ var spotify = new spotify(keys.spotify);
+ var client = new twitter(keys.twitter);
 
-//Make it so liri.js can take in one of the following commands: 
- //this.my-tweets = my-tweet;
-// this.spotify-this-song = spotify-this-song;
-// this.movie-this = movie-this;
+ //Make it so liri.js can take in one of the following commands: 
 
-//========================================================================
+ var Twitter = require('twitter');
 
-var Spotify = require('node-spotify-api')
-// var spotify = new Spotify(
-//     { 
-//         id: ("your spotify client id") 
-//         {
-//  secret: ("your spotify client secret")
+ var client = new Twitter({
+   consumer_key: '',
+   consumer_secret: '',
+   access_token_key: '',
+   access_token_secret: ''
+ });
+
+ var params = {
+   screen_name: 'nodejs'
+ };
+ client.get('statuses/user_timeline', params, function (error, tweets, response) {
+   if (!error) {
+     console.log("Tweets: " + JSON.parse(body));
+     // 1. 'node liri.js my-tweets'(display last 20 tweets and when they were create in the terminal bash) window
+
+     //========================================================================
+
+
+     var spotify = require('spotify')
+     var Spotify = require('node-spotify-api')
+     var spotify = new Spotify({
+         id: ("your spotify client id")
+       }
+     )}
+
+     secret: ("your spotify client secret") 
+
+   }); spotify.search({
+     type: 'track',
+     query: 'song name here',
+     artist: 'artist(s)',
+     link: 'preview link',
+     album: ' album',
+     limit: 20
+   })
+  
+   function (err, data) {
+     if (err) {
+
+       return console.log('Error occurred: ' + err);
+     }
     
-// });
-//  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {   if (err) {
-//  return console.log('Error occurred: ' + err);
-//   }
-//   console.log(data); 
-//  });
+     console.log("Track:" + JSON.parse(body).Track + "Song Name: " + JSON.parse(body). Twitter + "Artist(s): " + JSON.parse(body).Artist(s) + "link:"+ JSON.parse(body).Link + "Album:" + JSON.parse(body).Album + "Limit" + JSON.parse(body).Limit);
+    if (song < 20);
+    console.log("The Sign by Ace of Base: " + JSON.parse(body)) 
+    
+ });
+ //2.  'node  liri.js spotify -this-song '<song name here>' displays: Artist(s), The song's name, a preview link of the song from spotify.
+ // the album that the song is from. If no song is provided then the program will will default to "The Sign" by Ace of Base.
+ //======================================================================================
 
-//=======================================
+ // 'node liri.js movie-this 'movie name here'
+ //  If the user doesn't type a movie in the program will output data for the movie 'Mr. Nobody.'
 
-// Each command should do
+ // OMDB
+ var request = require("request");
 
-//1. 'node liri.js my-tweets'(display last 20 tweets and when they were create in the terminal bash) window
-//2.  'node  liri.js spotify -this-song '<song name here>' displays: Artist(s), The song's name, a preview link of the song from spotify.
-// the album that the song is from. If no song is provided then the program will will default to "The Sign" by Ace of Base.
+ // Grab the movieName 
+ var movieName = " ";
 
-// 'node liri.js movie-this 'movie name here'
-// This will output the following info to your terminal/bash window:
-//Title of the movie
-// Year the movie came out
-// IMDB rating of the movie.
-// Rotten tomatoes Rating of the movie.
-// Country where the movie wa produced.
-// Language of the movie
-// Plot of the movie
-// Actors in the movie
+ // Then run a request to the OMDB API with the movie specified
+ var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-//  If the user doesn't type a movie in the program will output data for the movie 'Mr. Nobody.'
-//=================================================
+ // This line is just to help us debug against the actual URL.
+ console.log(queryUrl);
 
+ request(queryUrl, function (error, response, body) {
+   console.log(JSON.parse(body));
 
+   // If the request is successful
+   if (!error && response.statusCode === 200) {
 
-// OMDB
-var request = require("request");
+     // Parse the body of the site and recover the data
+     console.log("Title: " + JSON.parse(body).Title + "Release Year: " + JSON.parse(body).Year + "IMDB Rating: " + JSON.parse(body).imdbRating + "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "Country where the movie was produced:" +
+       +JSON.parse(body).Country + "Language: " + JSON.parse(body).language + "Plot: " + JSON.parse(body).plot + "Actor: " + JSON.parse(body).actors
+     );
+   }
+ })
 
-// Grab the movieName which will always be the third node argument.
-var movieName = "Lion King";
-
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-request(queryUrl, function(error, response, body) {
-console.log(JSON.parse(body));
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Title: " + JSON.parse(body).Title + "Release Year: " + JSON.parse(body).Year + "IMDB Rating: " + JSON.parse(body).imdbRating + "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "Country where the movie was produced:" + 
-+ JSON.parse(body).Country +"Language: "  + JSON.parse(body).language + "Plot: "  + JSON.parse(body).plot + "Actor: " + JSON.parse(body).actors
-);
-}
-})
